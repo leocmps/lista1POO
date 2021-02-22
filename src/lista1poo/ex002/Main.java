f82b15bfff2ef7266c877c1682f85ddcc4e19fe0
leocmps <leocampos06@hotmail.com>
2021-02-22 19:44:25 -0300

    Adequação do exercício 02

--- src/lista1poo/ex002/Main.java
+++ src/lista1poo/ex002/Main.java
@@ -17,20 +17,49 @@ public class Main {
             switch (opc)
             {
                 case 1:
+                    aluno1 = new Aluno();
                     ra =  JOptionPane.showInputDialog("RA:");
                     nome =  JOptionPane.showInputDialog("Nome:");
-                    ac1 = Float.parseFloat(JOptionPane.showInputDialog(null, "AC1:"));
-                    ac2 = Float.parseFloat(JOptionPane.showInputDialog(null, "AC2:"));
-                    ag = Float.parseFloat(JOptionPane.showInputDialog(null, "AG:"));
-                    af = Float.parseFloat(JOptionPane.showInputDialog(null, "AF:"));
-                    aluno1 = new Aluno();
                     aluno1.setRa(ra);
                     aluno1.setNome(nome);
+                    
+                    ac1 = Float.parseFloat(JOptionPane.showInputDialog(null, "AC1:"));
                     aluno1.setAc1(ac1);
-                    aluno1.setAc2(ac2);
-                    aluno1.setAg(ag);
-                    aluno1.setAf(af);                                       
+                    if (ac1 <= 10 && ac1 >= 0)
+                    {                        
+                        ac2 = Float.parseFloat(JOptionPane.showInputDialog(null, "AC2:"));
+                        aluno1.setAc2(ac2);
+                        
+                        if(ac2 <= 10 && ac2 >= 0)
+                        {
+                            ag = Float.parseFloat(JOptionPane.showInputDialog(null, "AG:"));
+                            aluno1.setAg(ag);
+                            
+                            if(ag <= 10 && ag >= 0)
+                            {
+                                af = Float.parseFloat(JOptionPane.showInputDialog(null, "AF:"));
+                                aluno1.setAf(af);
+                                
+                                if(af <= 10 && af >= 0)
+                                {
+                                    
+                                }else{
+                                    JOptionPane.showMessageDialog(null, "Insira um valor de 0 á 10!", "Atenção", JOptionPane.WARNING_MESSAGE);
+                                }
+                            }else{
+                                JOptionPane.showMessageDialog(null, "Insira um valor de 0 á 10!", "Atenção", JOptionPane.WARNING_MESSAGE);
+                            }
+                        }else{
+                            JOptionPane.showMessageDialog(null, "Insira um valor de 0 á 10!", "Atenção", JOptionPane.WARNING_MESSAGE);
+                        }
+                    }else{
+                      JOptionPane.showMessageDialog(null, "Insira um valor de 0 á 10!", "Atenção", JOptionPane.WARNING_MESSAGE);  
+                    }
+                    
+                        
+                        
                     break;
+                    
                 case 2:
                     if(aluno1 == null)
                     {
@@ -50,3 +79,11 @@ public class Main {
         
     }
 }
+
+                        
+                                               
+
+            
+                    
+                                                         
+                    
